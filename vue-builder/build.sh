3f8004b62587ae15ee4665621f19f47c9d371498
Suzuki Takeo <takeo@stko.info>
2020-04-28 06:19:12 +0900

    fix build.sh

--- vue-builder/build.sh
+++ vue-builder/build.sh
@@ -3,5 +3,5 @@
 # 第一引数にpackage.jsonの実行場所からの相対パスをセット
 docker build `dirname $0` -t vue-builder:latest
 
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "${GOPATH}:/${GOPATH}" -w="/${PWD}" vue-builder:latest \
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "${PWD}:/${PWD}" -w="/${PWD}" vue-builder:latest \
 npm run build --prefix $1
