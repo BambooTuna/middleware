17b5aed4f7d5f78edb0096554d717b20095900db
Suzuki Takeo <takeo@stko.info>
2020-04-28 04:30:37 +0900

    code-genの docker build 修正

--- protoc-gen-grpc-web/code-gen.sh
+++ protoc-gen-grpc-web/code-gen.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-#docker build . -t protoc-gen-grpc-web:latest
+docker build `dirname $0` -t protoc-gen-grpc-web:latest
+
 alias grpc='docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "${GOPATH}:/${GOPATH}" -w="/${PWD}" protoc-gen-grpc-web'
 
 for file in `\find $1 -name '*.proto'`; do
