3648750cc4888af7bbac6ca314e3d9b994123199
Suzuki Takeo <takeo@stko.info>
2020-04-29 03:47:59 +0900

    fix proxy

--- go-vue/main.go
+++ go-vue/main.go
@@ -6,7 +6,6 @@ import (
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 	"log"
-	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"strings"
@@ -16,13 +15,13 @@ func main() {
 	serverPort := config.GetEnvString("PORT", "9090")
 	r := gin.Default()
 
-	target := config.GetEnvString("API_SERVER_ENDPOINT", "http://localhost:18080")
-	r.Use(reverseProxy("/v1", target))
-
 	r.Use(static.Serve("/", static.LocalFile("./dist", false)))
 	r.NoRoute(func(c *gin.Context) {
 		c.File("./dist/index.html")
 	})
+
+	target := config.GetEnvString("API_SERVER_ENDPOINT", "http://localhost:18080")
+	r.Use(reverseProxy("/v1", target))
 
 	log.Fatal(r.Run(fmt.Sprintf(":%s", serverPort)))
 }
@@ -33,8 +32,6 @@ func reverseProxy(urlPrefix string, target string) gin.HandlerFunc {
 		log.Println("Reverse Proxy target url could not be parsed:", err)
 		return nil
 	}
-
-	http.Handle("/", httputil.NewSingleHostReverseProxy(url))
 	proxy := httputil.NewSingleHostReverseProxy(url)
 	return func(c *gin.Context) {
 		if strings.HasPrefix(c.Request.URL.Path, urlPrefix) {
