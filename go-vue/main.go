2f6c557fb8657a3600492b0d3e11e4a07017bb03
Suzuki Takeo <takeo@stko.info>
2020-04-29 05:30:47 +0900

    refactor

--- go-vue/main.go
+++ go-vue/main.go
@@ -8,7 +8,6 @@ import (
 	"log"
 	"net/http/httputil"
 	"net/url"
-	"strings"
 )
 
 func main() {
@@ -20,10 +19,15 @@ func main() {
 		c.File("./dist/index.html")
 	})
 
-	target := config.GetEnvString("API_SERVER_ENDPOINT", "http://localhost:18080")
-	r.Use(reverseProxy("/v1", target))
+	go func() {
+		log.Fatal(r.Run(fmt.Sprintf(":%s", serverPort)))
+	}()
 
-	log.Fatal(r.Run(fmt.Sprintf(":%s", serverPort)))
+	api := gin.Default()
+	target := config.GetEnvString("API_SERVER_ENDPOINT", "http://localhost:18080")
+	api.Use(reverseProxy("", target))
+	log.Fatal(r.Run(fmt.Sprintf(":%s", "8080")))
+
 }
 
 func reverseProxy(urlPrefix string, target string) gin.HandlerFunc {
@@ -34,9 +38,10 @@ func reverseProxy(urlPrefix string, target string) gin.HandlerFunc {
 	}
 	proxy := httputil.NewSingleHostReverseProxy(url)
 	return func(c *gin.Context) {
-		if strings.HasPrefix(c.Request.URL.Path, urlPrefix) {
-			c.Request.URL.Path = strings.Replace(c.Request.URL.Path, urlPrefix, "", 1)
-			proxy.ServeHTTP(c.Writer, c.Request)
-		}
+		//if strings.HasPrefix(c.Request.URL.Path, urlPrefix) {
+		//	c.Request.URL.Path = strings.Replace(c.Request.URL.Path, urlPrefix, "", 1)
+		//	proxy.ServeHTTP(c.Writer, c.Request)
+		//}
+		proxy.ServeHTTP(c.Writer, c.Request)
 	}
 }
