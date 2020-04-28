60309f0d1a419ab80a976edde3722bc2708582e8
Suzuki Takeo <takeo@stko.info>
2020-04-28 09:03:05 +0900

    refactor

--- vue-builder/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# 第一引数にpackage.jsonの実行場所からの相対パスをセット
-docker build `dirname $0` -t vue-builder:latest
-
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "${PWD}:/${PWD}" -w="/${PWD}" vue-builder:latest \
-npm run build --prefix $1
