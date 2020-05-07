686a9e19c36b4b4f3c564d35aff1a2c9e9ac6fdd
Suzuki Takeo <takeo@stko.info>
2020-05-07 14:29:19 +0900

    refactor

--- go-vue/main.go
+++ go-vue/main.go
@@ -3,42 +3,19 @@ package main
 import (
 	"fmt"
 	"github.com/BambooTuna/go-server-lib/config"
-	//"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 	"log"
-	"net/http/httputil"
-	"net/url"
-	"strings"
 )
 
 func main() {
 	serverPort := config.GetEnvString("PORT", "8080")
-	target := config.GetEnvString("API_SERVER_ENDPOINT", "http://localhost:18080")
 
 	r := gin.Default()
-	//r.Use(cors.Default())
-	r.Use(reverseProxy("/api", target))
 	r.Use(static.Serve("/", static.LocalFile("./dist", false)))
 	r.NoRoute(func(c *gin.Context) {
 		c.File("./dist/index.html")
 	})
 
 	log.Fatal(r.Run(fmt.Sprintf(":%s", serverPort)))
-}
-
-func reverseProxy(urlPrefix string, target string) gin.HandlerFunc {
-	url, err := url.Parse(target)
-	if err != nil {
-		log.Println("Reverse Proxy target url could not be parsed:", err)
-		return nil
-	}
-	proxy := httputil.NewSingleHostReverseProxy(url)
-	proxy.FlushInterval = -1
-	return func(c *gin.Context) {
-		if strings.HasPrefix(c.Request.URL.Path, urlPrefix) {
-			c.Request.URL.Path = strings.Replace(c.Request.URL.Path, urlPrefix, "", 1)
-			proxy.ServeHTTP(c.Writer, c.Request)
-		}
-	}
 }
